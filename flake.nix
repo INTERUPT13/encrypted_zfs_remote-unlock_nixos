10212a764ed6a33b206ac3ba4f92f99e7c461bdb
root <root@dontspamme.com>
2022-09-10 12:48:04 +0000

    first warningless working build

--- flake.nix
+++ flake.nix
@@ -3,10 +3,24 @@
 
   inputs = {
     nixpkgs = { url = "github:nixos/nixpkgs/nixos-unstable"; };
+    security-cfg = { 
+      url = "git+ssh://git@github.com/INTERUPT13/encrypted_zfs_remote-unlock_nixos_security.git";
+      flake = false;
+    };
+    # optional you can also use a ./security.cfg in your /etc/nixos/ folder (see below)
+    # but i go with a priv git repo flake
+    hardware-cfg = { 
+      url = "git+ssh://git@github.com/INTERUPT13/encrypted_zfs_remote-unlock_nixos_hardware.git";
+      flake = false;
+    };
+
   };
 
-  outputs = { self, nixpkgs}@attrs: with nixpkgs; let
+  outputs = { self, nixpkgs, security-cfg, hardware-cfg}@attrs: with nixpkgs; let
+    # todo splitin modules
     pub_cfg = {config, pkgs, ...}: {
+      system.stateVersion = "22.05";
+
       boot.loader.grub.enable = true;
       boot.loader.grub.version = 2;
       services.zfs.autoScrub.enable = true;
@@ -15,15 +29,18 @@
       #on desktop maybe set os prober -> multiboot for winshit
       boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
 
-      # TODO move such things as packages in their own flake/module
       environment.systemPackages = with pkgs; [
         mosh
       ];
 
-      system.stateVersion = "22.05"; # Did you read the comment?
+      services.openssh.enable = true;
+      # TODO cert only
+      services.openssh.permitRootLogin = "yes";
 
-      # protect agaainst accidental configuration.nix deletions TODO
-      # find out why this doesn't work with pure eval
+      # TODO disable. Acutally put everything like sshd + fw in a "security flake thats not public
+      networking.firewall.enable = false;
+
+      # protect agaainst accidental configuration.nix deletions
       #system.copySystemConfiguration = true;
     };
   in {
@@ -31,14 +48,16 @@
       system = "x86_64-linux";
       # to pass flake inputs to modules if needed
       modules = [ 
-        ./searx.nix 
-        ./hardware-configuration.nix
         pub_cfg
-
+        
         # EXAMPLE FOUND IN ./security.nix.example
         # security relevant stuff. I wont share my actual config but just think of it
         # as a bunch of firewall,selinux whatever settings
-        ./security.nix
+        #./security.nix
+        (import "${security-cfg}/security-modules.nix")
+        (import "${hardware-cfg}/hardware-modules.nix")
+        # ^ using my own priv repos but you can just put it in your /etc/nixos
+        # and source via ./<file>.nix TODO guide on how to generate hardware-configuration.nix
       ];
       specialArgs = attrs;
     };
